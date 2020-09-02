c91daa86f0c7cc7c259ba0dd36c044703e278507
chenpy <chenpy@knownsec.com>
2020-09-02 13:02:01 +0800

    feat: 循环

---
+++ basicConcept/c4.py
@@ -0,0 +1,27 @@
+'''
+包、模块、函数
+'''
+
+# arr = ['apple', 'orange', 'banana']
+# for x in arr:
+#     print(x)
+
+# arr = [['apple','orange', 'banana'], (1,2,3,4,5,6)]
+
+# for x in arr:
+#     for y in x:
+#         print(y, end='')
+# else:
+#     print('please check result')
+
+
+# for num in range(0, 10, 2):
+#     print(num)
+
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+# for el in range(0, len(arr), 2):
+#     print(arr[el])
+
+b = arr[0:len(arr):2] 
+print(b)
