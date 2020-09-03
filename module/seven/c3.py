ebf11dc9628275c8778a02f25bb7678e92dd7b4b
chenpy <chenpy@knownsec.com>
2020-09-03 13:58:11 +0800

    feat: 包,模块

---
+++ module/seven/c3.py
@@ -0,0 +1,24 @@
+
+'''
+导入模块，使用模块变量的时候需要遵循模块.变量
+'''
+# import c4
+# import newModule.c5
+
+# print(c4.a)
+# print(newModule.c5.b) 
+
+
+'''
+从模块导入变量
+'''
+
+# from newModule.c5 import b
+# print(b)
+
+
+from newModule.c5 import *
+print(b)
+print(c)
+
+
