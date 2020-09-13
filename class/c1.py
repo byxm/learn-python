31059cdbe3300770fd64841af4525dd49df67f23
陈攀宇 <panyuximeng@163.com>
2020-09-13 19:46:27 +0800

    feat: 类变量和实例变量

--- class/c1.py
+++ class/c1.py
@@ -1,13 +1,26 @@
 
 class Student():
-    name = ''
+    name = '' # 类变量
     age = 0
 
+    def __init__(self, name, age):
+        self.name = name # 实例变量
+        self.age = age
+        print('class Name age', self.name,self.age)
+
     def print_file(self):
-        print('name', self.name)
+        self.name = 'changeSimone'
         print('age', self.age)
 
 
-student = Student()
+student = Student('simone', 18)
+print(student.name)
 student.print_file()
+print(student.name)
 
+print('Student',Student.name) # 类变量直接通过类来调用，区别于js里面的需要通过指定static为静态变量才能调用
+
+
+
+
+# print(id(student1), id(student2), id(student3))
